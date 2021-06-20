cbcd0b74d95071601a692f2722ffbf7eb2f09a9c
emerson-acacio-fs <emerson.acacio2@gmail.com>
2021-06-20 18:08:23 -0300

    type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { type } from 'os';
 import { stringify } from 'querystring';
 
 function gerarPessoa(
@@ -19,3 +20,13 @@ enum Cores {
 }
 
 console.log(Cores.VERMELHO);
+
+type Idade = number;
+type Pessoa = { nome: string; idade: Idade; salario?: number };
+
+type CorRGB = 'vermelho' | 'verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'wwww';
+
+type Final = CorRGB | CorCMYK;
+
+const pessoa: Pessoa = { idade: 1, nome: 'dddd', salario: 444 };
